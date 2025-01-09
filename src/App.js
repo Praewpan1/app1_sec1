eed4e3b794644bb4b515250324a1054f20ddecb6
Praewpan1 <152370482+Praewpan1@users.noreply.github.com>
2025-01-09 11:02:52 +0700

    function-components
    function-components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-import './style.css'
-let r ='React'
-let rn = 'React Native'
-const el =(
-  <div>
-    Create Web & Mobile Apps <br/>
-    with{r} & {rn}
-  </div>
-)
+import{Header,Content,Footer} from './func-components'
 
 function App() {
   return (
-    <>
-    <h2>DeveloperThai.com</h2>
-    <div className="title">React & React Native</div>
-    </>
+   [<Header/>,<Content/>,<Footer/>]
   )
   
 }
