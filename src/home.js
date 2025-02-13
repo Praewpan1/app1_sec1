7a101d6d8ba61a1b4733fa31c00c46e6e507fcae
Praewpan1 <152370482+Praewpan1@users.noreply.github.com>
2025-02-13 11:30:45 +0700

    Update home.js

--- src/home.js
+++ src/home.js
@@ -7,7 +7,7 @@ export default function Home(){
             รายชื่อ <a href="/products">สินค้า</a> ที่เรามีจำหน่าย
             <br/>
             แต่ท่านต้องเป็น <a href="/member">สมาชิก</a> จึงจะสั่งซื้อได้<br/>
-            หากมีข้อสงสัย กรุณา<a href="/contact">ติดต่อเรา</a>
+            หากมีข้อสงสัย กรุณา <a href="/contact">ติดต่อเรา</a>
         </p>
        
     )
